0a295011968c04e71bcac530e7fb4fff190d32f3
Sylvain Rivière <sylv1.riviere@gmail.com>
2021-03-10 16:12:25 +0400

    add: highlight todo type to display

--- src/components/sortTodoList/SortToDoList.js
+++ src/components/sortTodoList/SortToDoList.js
@@ -1,20 +1,69 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { active, all, completed } from '../../constant/constant';
 import { TodoContext } from '../../contexts/TodoContext';
 import * as styles from './sortTodoList.module.css';
 
 const SortToDoList = () => {
+    const [iselected, setIsSelected] = useState({
+        all: true,
+        active: false,
+        completed: false,
+    });
     const { changeTodosToDisplay } = useContext(TodoContext);
 
     const clickHandler = (key) => {
         changeTodosToDisplay(key);
+        howIsSelected(key);
+    };
+
+    const howIsSelected = (key) => {
+        switch (key) {
+            case all:
+                setIsSelected({
+                    all: true,
+                    active: false,
+                    completed: false,
+                });
+                break;
+            case active:
+                setIsSelected({
+                    all: false,
+                    active: true,
+                    completed: false,
+                });
+                break;
+            case completed:
+                setIsSelected({
+                    all: false,
+                    active: false,
+                    completed: true,
+                });
+                break;
+            default:
+                console.error('wrong key');
+        }
     };
 
     return (
         <div className={styles.sortToDoList}>
-            <div onClick={() => clickHandler(all)}>All</div>
-            <div onClick={() => clickHandler(active)}>Active</div>
-            <div onClick={() => clickHandler(completed)}>Completed</div>
+            <div
+                className={iselected.all ? styles.selected : ''}
+                onClick={() => clickHandler(all)}
+            >
+                All
+            </div>
+            <div
+                className={iselected.active ? styles.selected : ''}
+                onClick={() => clickHandler(active)}
+            >
+                Active
+            </div>
+            <div
+                className={iselected.completed ? styles.selected : ''}
+                onClick={() => clickHandler(completed)}
+            >
+                Completed
+            </div>
         </div>
     );
 };
