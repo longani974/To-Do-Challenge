d77d5f231d3fd2b9d5624915201bd7332834cabe
Sylvain Rivière <sylv1.riviere@gmail.com>
2021-03-10 08:32:04 +0400

    add: send todo to localstorage and getItem on refresh

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -1,22 +1,38 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 export const TodoContext = createContext();
 
 const TodoContextProvider = (props) => {
-    const [todoDatas, setTodoDatas] = useState([
-        { id: 'fds', todo: '1Jog around the park 5x' },
-        { id: 'fdqs', todo: '2Jog around the park 5x' },
-        { id: 'fcds', todo: '3Jog around the park 5x' },
-        { id: 'fdbs', todo: '4Jog around the park 5x' },
-        { id: 'fdns', todo: '5Jog around the park 5x' },
-    ]);
+    const [todoDatas, setTodoDatas] = useState([]);
+
+    useEffect(() => {
+        const localStorageTodo = JSON.parse(localStorage.getItem('todoList'));
+        console.log(localStorageTodo.length);
+        if (localStorageTodo.length > 0) {
+            setTodoDatas(localStorageTodo);
+        } else {
+            setTodoDatas([
+                { id: 'fds', todo: '1Jog around the park 5x' },
+                { id: 'fdqs', todo: '2Jog around the park 5x' },
+                { id: 'fcds', todo: '3Jog around the park 5x' },
+                { id: 'fdbs', todo: '4Jog around the park 5x' },
+                { id: 'fdns', todo: '5Jog around the park 5x' },
+            ]);
+        }
+    }, []);
 
     const addTodo = (todo) => {
-        setTodoDatas([todo, ...todoDatas]);
+        const newDataList = [todo, ...todoDatas];
+        setTodoDatas(newDataList);
+        localStorage.setItem('todoList', JSON.stringify(newDataList));
+        // console.log(JSON.parse(localStorage.getItem('todoList')));
     };
 
     const deleteTodo = (todoId) => {
-        setTodoDatas([...todoDatas].filter((data) => data.id !== todoId));
+        const newDataList = [...todoDatas].filter((data) => data.id !== todoId);
+        setTodoDatas(newDataList);
+        localStorage.setItem('todoList', JSON.stringify(newDataList));
+        // console.log(JSON.parse(localStorage.getItem('todoList')));
     };
 
     return (
