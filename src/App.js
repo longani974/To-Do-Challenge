06c2e42f28dcdcf35a53fc5bd6d535b8e694fc38
Sylvain Rivière <sylv1.riviere@gmail.com>
2021-04-19 10:56:58 +0400

    change: dark mode by default

--- src/App.js
+++ src/App.js
@@ -8,50 +8,50 @@ import SortToDoList from './components/sortTodoList/SortToDoList';
 import Footer from './components/footer/Footer';
 
 function App() {
-    const darkMode = 'dark';
-    const lightMode = 'light';
-    const [lightOrDark, setlightOrDark] = useState('');
+  const darkMode = 'dark';
+  const lightMode = 'light';
+  const [lightOrDark, setlightOrDark] = useState('');
 
-    useEffect(() => {
-        document.querySelector('body').className = lightOrDark;
-    }, [lightOrDark]);
+  useEffect(() => {
+    document.querySelector('body').className = lightOrDark;
+  }, [lightOrDark]);
 
-    useEffect(() => {
-        if (localStorage.getItem('lightOrDarkMode')) {
-            setlightOrDark(localStorage.getItem('lightOrDarkMode'));
-        } else {
-            setlightOrDark('light');
-        }
-    }, []);
+  useEffect(() => {
+    if (localStorage.getItem('lightOrDarkMode')) {
+      setlightOrDark(localStorage.getItem('lightOrDarkMode'));
+    } else {
+      setlightOrDark('dark');
+    }
+  }, []);
 
-    const lightOrDarkModeHandler = () => {
-        if (lightOrDark === lightMode) {
-            changeDarkOrLightMode(darkMode);
-        } else {
-            changeDarkOrLightMode(lightMode);
-        }
-    };
+  const lightOrDarkModeHandler = () => {
+    if (lightOrDark === lightMode) {
+      changeDarkOrLightMode(darkMode);
+    } else {
+      changeDarkOrLightMode(lightMode);
+    }
+  };
 
-    const changeDarkOrLightMode = (mode) => {
-        setlightOrDark(mode);
-        localStorage.setItem('lightOrDarkMode', mode);
-    };
+  const changeDarkOrLightMode = (mode) => {
+    setlightOrDark(mode);
+    localStorage.setItem('lightOrDarkMode', mode);
+  };
 
-    return (
-        <div className="App">
-            <TodoContextProvider>
-                <div className="wrapperApp">
-                    <Header lightOrDarkMode={lightOrDarkModeHandler} />
-                    <AddTask />
-                    <TodoList />
-                    <div className="sortToDoList">
-                        <SortToDoList />
-                    </div>
-                    <Footer />
-                </div>
-            </TodoContextProvider>
+  return (
+    <div className="App">
+      <TodoContextProvider>
+        <div className="wrapperApp">
+          <Header lightOrDarkMode={lightOrDarkModeHandler} />
+          <AddTask />
+          <TodoList />
+          <div className="sortToDoList">
+            <SortToDoList />
+          </div>
+          <Footer />
         </div>
-    );
+      </TodoContextProvider>
+    </div>
+  );
 }
 
 export default App;
